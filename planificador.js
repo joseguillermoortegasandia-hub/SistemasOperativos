d0c4ed7b5919e8bbe3ef8a7f14839f54c60cbc55
joseguillermoortegasandia-hub <joseguillermoortegasandia@gmail.com>
2026-04-10 09:55:42 -0400

    Update planificador.js
    funcion para exportarlo

--- planificador.js
+++ planificador.js
@@ -15,3 +15,20 @@ function calcularMetricas(p) {
     p.E = p.T - p.t;
     p.I = p.T !== 0 ? p.t / p.T : 0;
 }
+
+function generarReporte(nombre, procesos) {
+    let contenido = `REPORTE DE METODO: ${nombre} (Q=4)\n`;
+    contenido += `ID\tTi\tt\tTf\tT\tE\tI\n`;
+    let sT = 0, sE = 0, sI = 0;
+    
+    procesos.forEach(p => {
+        contenido += `${p.id}\t${p.ti}\t${p.t}\t${p.tf.toFixed(2)}\t${p.T.toFixed(2)}\t${p.E.toFixed(2)}\t${p.I.toFixed(2)}\n`;
+        sT += p.T; sE += p.E; sI += p.I;
+    });
+
+    const n = procesos.length;
+    contenido += `\nPROMEDIOS:\nT_prom: ${(sT/n).toFixed(2)}\nE_prom: ${(sE/n).toFixed(2)}\nI_prom: ${(sI/n).toFixed(2)}\n`;
+    
+    fs.writeFileSync(`resultado_${nombre}.txt`, contenido);
+    return sE / n;
+}
